e669a042974051d6fc76dd90f6a036cb80639ced
Aleksi Pamilo <aleksi@pmlo.dev>
2026-08-22 21:50:01 +0300

    enforce trailingHash: never

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kurssi.pmlo.dev',
+  trailingSlash: 'never',
   vite: {
     plugins: [tailwindcss()]
   }
